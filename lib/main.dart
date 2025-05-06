77738a7a0dc29e19e7e13140623ef427815c2441
annarafafo <annarafafo@gmail.com>
2025-05-05 23:35:21 -0300

    add dotenv support and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:histocr_app/providers/auth_provider.dart';
 import 'package:histocr_app/screens/complete_profile_screen.dart';
@@ -10,9 +11,11 @@ import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  await dotenv.load();
+  
   await Supabase.initialize(
-    url: supabaseUrl,
-    anonKey: supabaseAnonKey,
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
   runApp(
     MultiProvider(
